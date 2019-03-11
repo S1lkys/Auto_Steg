b3aa8d5985b72dc6a5502992d7e973e6af70b46b
Silky <40408435+S1lkys@users.noreply.github.com>
2019-03-11 21:33:40 +0100

    Add files via upload

---
+++ auto_steg.sh
@@ -0,0 +1,11 @@
+read -p $'\e[1;92m JPG-FILE: \e[0m' picture
+read -p $'\e[1;92m List with passwords: \e[0m' wl_pass 
+wl_pass="${wl_pass}"
+picture="${picture}"
+
+count_pass=$(wc -l $wl_pass | cut -d " " -f1)
+
+for fn in `cat $wl_pass`; do
+steghide extract -sf $picture -p $fn
+done
+
